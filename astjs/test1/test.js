5928ee6463b6d6d1c1a844c34250c336518d5fa5
wanghauiyao <741663036@qq.com>
2020-08-29 09:32:53 +0800

    dev

---
+++ astjs/test1/test.js
@@ -0,0 +1,123 @@
+let res = [];
+
+function log(value) {
+    res.push(value);
+    console.log(value);
+}
+
+function logResStr() {
+    let res2 = JSON.stringify(res);
+    res2 = JSON.parse(res2).sort().join("");
+    console.log(res2);
+
+}
+
+function logRes() {
+    
+    console.log('res2');
+
+}
+
+function getStr1() {
+    return 11 + "ee";
+}
+
+function getNumber() {
+    return 11;
+}
+
+function getBool1() {
+    return 2;
+}
+
+function getBool2() {
+    return 2;
+}
+
+function getNumber2() {
+    return 2;
+}
+
+function getNumber3() {
+    return 2;
+}
+
+function getNumber4() {
+    return 2;
+}
+
+function getNumberCD() {
+    return 2;
+}
+
+function len(str) {
+    return str.length;
+}
+
+function toInt(str) {
+    return parseInt(str);
+}
+
+function getNumber5() {
+    return 50;
+}
+
+function getNumber6() {
+    return 50;
+}
+
+function getNumber7() {
+    return 50;
+}
+
+function getBool7() {
+    return true;
+}
+
+function getBool8() {
+    return true;
+}
+
+function getBool9() {
+    return true;
+}
+
+function getBool10() {
+    return true;
+}
+
+function getBool11() {
+    return true;
+}
+function getBool12() {
+    return true;
+}
+function getBool13() {
+    return true;
+}
+function getBool14() {
+    return true;
+}
+function getBool15() {
+    return true;
+}
+
+function getBool16() {
+    return true;
+}
+
+function getBool17() {
+    return true;
+}
+
+function getNumberE() {
+    return true;
+}
+
+function getNumberF() {
+    return true;
+}
+
+function getNumberOffset() {
+    return true;
+}
---
+++ astjs/test3/test.js
@@ -0,0 +1,305 @@
+var res = [];
+
+function log(value) {
+    res.push(value);
+    console.log(value);
+}
+function len(str) {
+    return str.length;
+}
+function logResStr() {
+    var res2 = JSON.stringify(res);
+    res2 = JSON.parse(res2).sort().join("");
+    console.log(res2);
+
+}
+
+function logRes() {
+    console.log('res2');
+}
+
+function getBool1() {
+    return true;
+}
+function getBool2() {
+    return true;
+}
+function getBool3() {
+    return true;
+}
+function getBool4() {
+    return true;
+}
+function getBool5() {
+    return true;
+}
+function getBool6() {
+    return true;
+}
+function getBool7() {
+    return true;
+}
+function getBool8() {
+    return true;
+}
+
+function getBool9() {
+    return true;
+}
+
+function getBool10() {
+    return true;
+}
+
+function getBool11() {
+    return true;
+}
+
+function getBool12() {
+    return true;
+}
+
+function getBool13() {
+    return true;
+}
+function getBool14() {
+    return true;
+}
+function getBool15() {
+    return true;
+}
+function getBool16() {
+    return true;
+}
+function getBool17() {
+    return true;
+}
+function getBool18() {
+    return true;
+}
+function getBool19() {
+    return true;
+}
+function getBool20() {
+    return true;
+}
+
+function getBool21() {
+    return true;
+}
+
+function getBool22() {
+    return true;
+}
+
+function getBool23() {
+    return true;
+}
+
+function getBool24() {
+    return true;
+}
+
+function getBool25() {
+    return true;
+}
+
+function getBool26() {
+    return true;
+}
+
+function getBool27() {
+    return true;
+}
+
+function getBool28() {
+    return true;
+}
+
+function getBool29() {
+    return true;
+}
+
+function getBool31() {
+    return true;
+}
+
+function getBool32() {
+    return true;
+}
+
+function getBool33() {
+    return true;
+}
+
+function getBool34() {
+    return true;
+}
+
+function getBool35() {
+    return true;
+}
+
+function getBool36() {
+    return true;
+}
+
+function getBool37() {
+    return true;
+}
+
+function getBool38() {
+    return true;
+}
+
+function getBool39() {
+    return true;
+}
+
+function getBool41() {
+    return true;
+}
+
+function getBool42() {
+    return true;
+}
+
+function getBool43() {
+    return true;
+}
+
+function getBool44() {
+    return true;
+}
+
+function getBool45() {
+    return true;
+}
+
+function getBool46() {
+    return true;
+}
+
+function getBool47() {
+    return true;
+}
+
+function getBool48() {
+    return true;
+}
+
+function getBool49() {
+    return true;
+}
+
+function getBool50() {
+    return true;
+}
+
+function getBool51() {
+    return true;
+}
+
+function getBool52() {
+    return true;
+}
+
+function getBool53() {
+    return true;
+}
+
+function getBool54() {
+    return true;
+}
+
+function getBool55() {
+    return true;
+}
+
+function getBool56() {
+    return true;
+}
+
+function getBool57() {
+    return true;
+}
+
+function getNumber1() {
+    return 1;
+}
+
+function getNumber2() {
+    return 1;
+}
+
+function getNumber3() {
+    return 1;
+}
+
+function getNumber4() {
+    return 1;
+}
+
+function getNumber5() {
+    return 1;
+}
+
+function getNumber6() {
+    return 1;
+}
+
+function getNumber7() {
+    return 1;
+}
+
+function getNumber8() {
+    return 1;
+}
+
+function getNumber9() {
+    return 1;
+}
+
+function getNumber10() {
+    return 1;
+}
+
+function getNumber11() {
+    return 1;
+}
+
+function getNumber12() {
+    return 1;
+}
+
+function getNumber13() {
+    return 1;
+}
+
+function getNumber14() {
+    return 1;
+}
+
+function getNumber15() {
+    return 1;
+}
+
+function getNumber16() {
+    return 1;
+}
+
+function getNumber17() {
+    return 1;
+}
+
+function getNumber18() {
+    return 1;
+}
+
+function getNumber19() {
+    return 1;
+}
+
+function getNumber20() {
+    return 1;
+}
